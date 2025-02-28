e3b4b8e20620470bb17559a4f745ac946ac768cc
SpeedAmigo <Kxipi7@gmail.com>
2025-02-28 17:10:55 +0100

    flappy cake and texture change

---
+++ Assets/Flappy-cake/Scripts/PieScript.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class PieScript : MonoBehaviour
+{
+    private Rigidbody2D _body;
+    private PolygonCollider2D _collider;
+    private bool _isAlive = true;
+    [SerializeField] private bool godMode;
+    [SerializeField] private int _flyForce;
+    [SerializeField] private float _maxSpeed;
+    [SerializeField] private float _rotationSpeed;
+    [SerializeField] PlayableDirector _director;
+    
+    private void Fly()
+    {
+        _body.AddForce(Vector3.up * (_flyForce * 100));
+
+        if (_body.linearVelocity.magnitude > _maxSpeed)
+        {
+            _body.linearVelocity = _body.linearVelocity.normalized * _maxSpeed;
+        }
+    }
+
+    private void Die()
+    {
+        if (godMode) return;
+        _collider.enabled = false;
+        _isAlive = false;
+        _director.Play();
+    }
+
+    private void PieRotation()
+    {
+        float pieVelocity = _body.linearVelocity.y;
+        
+        // Determine target angle based on upward or downward movement
+        float targetAngle = Mathf.Clamp(pieVelocity * _rotationSpeed, -20f, 20f);
+
+        // Smoothly rotate towards the target angle
+        Quaternion targetRotation = Quaternion.Euler(0, 0, targetAngle);
+        gameObject.transform.rotation = Quaternion.Lerp(
+            gameObject.transform.rotation,
+            targetRotation, 
+            Time.deltaTime * _rotationSpeed);
+    }
+    
+    private void Start()
+    {
+        _body = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<PolygonCollider2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _isAlive)
+        {
+            Fly();
+        }
+
+        if (transform.position.y is < -6 or > 6) // this is the same as || statement
+        {
+            Die();
+        }
+        
+        PieRotation();
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Die();
+    }
+}
---
+++ Assets/Flappy-cake/Scripts/PipeLogic.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class PipeLogic : MonoBehaviour
+{
+    private Rigidbody2D _body;
+    private ObjectPool<PipeLogic> _pipePool;
+    
+    [HideInInspector]
+    public GameManagerScript gameManager;
+    
+    [Tooltip("Speed of the pipe")]
+    [SerializeField] private float _pipeSpeed;
+    
+    [Tooltip("Indicates how much points to add")]
+    [SerializeField] private int _pipeScoreValue;
+    
+
+    public void SetPool(ObjectPool<PipeLogic> pool)
+    {
+        _pipePool = pool;
+    }
+    
+    void Start()
+    {
+      _body = GetComponent<Rigidbody2D>();  
+    }
+    
+    private void FixedUpdate()
+    {
+        _body.linearVelocity = new Vector2((-1 * _pipeSpeed), 0);
+
+        if (gameObject.transform.position.x <= -14f)
+        {
+            _pipePool.Release(this);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        gameManager.AddScorePoint(_pipeScoreValue);
+    }
+}

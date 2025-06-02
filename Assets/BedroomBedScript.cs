12f8854fc2a02a814cd9c0ef5fd32467509e5fe8
SpeedAmigo <Kxipi7@gmail.com>
2025-06-02 17:48:28 +0200

    text sequence wip

--- Assets/BedroomBedScript.cs
+++ Assets/BedroomBedScript.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using DG.Tweening;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,6 +9,12 @@ public class BedroomBedScript : MinigameManager
 {
     [SerializeField] private Image image;
     [SerializeField] private DayNightScript globalTimer;
+
+    private bool waitingForInput;
+    private Sequence sequence;
+    private Sequence textSequence;
+    
+    public List<DayBeginText> dayBeginTexts = new();
     
     public override void Interact()
     {
@@ -14,15 +23,6 @@ public class BedroomBedScript : MinigameManager
         if (globalTimer.CurrentDayCycle == DayCycles.Night)
         {
             ImageAnimation();
-            
-            globalTimer.SetDayCycle(DayCycles.Sunrise);
-            
-            if (globalTimer.GetDayCount() < 7)
-            {
-                globalTimer.IncreaseDayCount();
-            }
-
-            Debug.Log("Day Count: " + globalTimer.GetDayCount());
         }
         else
         {
@@ -34,12 +34,68 @@ public class BedroomBedScript : MinigameManager
     {
         image.gameObject.SetActive(true);
         
-        Color color = image.color;
-        color.a = 1;
-
-        image.DOColor(color, 1f)
+        sequence = DOTween.Sequence();
+        
+        sequence.Append(image.DOFade(1f, 1f)
             .SetEase(Ease.InOutSine)
-            .SetLoops(2, LoopType.Yoyo)
-            .OnComplete(() => image.gameObject.SetActive(false));
+            .OnStepComplete(() =>
+            { 
+                ChangeDayCycle();
+                waitingForInput = true;
+                sequence.Pause();
+            }))
+            .Append(image.DOFade(0f, 1f));
     }
+
+    private void StartTextSequence()
+    {
+        textSequence = DOTween.Sequence();
+
+        DayBeginText targetText = new();
+
+        foreach (var dayBeginText in dayBeginTexts)
+        {
+            if (dayBeginText.textForDay == globalTimer.GetDayCount())
+            {
+                targetText = dayBeginText;
+            }
+        }
+
+        foreach (var text in targetText.textList)
+        {
+            text.gameObject.SetActive(true);
+
+            textSequence.Append(text.DOFade(1f, 1f));
+            textSequence.AppendInterval(1f);
+        }
+    }
+    
+    private void ChangeDayCycle()
+    {
+        globalTimer.SetDayCycle(DayCycles.Sunrise);
+            
+        if (globalTimer.GetDayCount() < 7)
+        {
+            globalTimer.IncreaseDayCount();
+        }
+
+        Debug.Log("Day Count: " + globalTimer.GetDayCount());
+    }
+
+    private void Update()
+    {
+        if (waitingForInput && Input.GetKeyDown(KeyCode.Space))
+        {
+            waitingForInput = false;
+            sequence.Play();
+        }
+    }
+}
+
+[Serializable]
+public class DayBeginText
+{
+    public List<TMP_Text> textList;
+    public TMP_Text skipText;
+    public int textForDay;
 }

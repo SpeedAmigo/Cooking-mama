a13e87a5b2c2f9c90997593a9c7247ef9ab4e170
SpeedAmigo <Kxipi7@gmail.com>
2025-06-09 12:48:34 +0200

    Garden access on day count

--- Assets/Scripts/StairsController.cs
+++ Assets/Scripts/StairsController.cs
@@ -6,10 +6,20 @@ using UnityEngine.UI;
 
 public class StairsController : MonoBehaviour
 {
+    [TabGroup("Dependencies")]
     [SerializeField] private Image transitionImage;
+    [TabGroup("Dependencies")]
+    [SerializeField] private DayNightScript dayNightScript;
+    [TabGroup("Dependencies")]
     [SerializeField] private SO_TransitionProperties properties;
+    [TabGroup("Properties")]
     [SceneDropdown] [SerializeField] private string sceneName;
+    [TabGroup("Properties")]
     [SerializeField] [MinValue(1)] private int spawnPointIndex;
+    [TabGroup("Properties")]
+    [SerializeField] private float unlockOnDay;
+    
+    
     
     private bool canTransition = true;
     public bool CanTransition { set {canTransition = value; }}
@@ -25,6 +35,7 @@ public class StairsController : MonoBehaviour
         if (collision.TryGetComponent(out PlayerScript player))
         {
             if (!canTransition) return;
+            if (unlockOnDay > dayNightScript.GetDayCount()) return;
             
             TransitionManager.IsTransitioning = true;
             GameStateManager.ChangeGameState(GameState.SceneLoading);

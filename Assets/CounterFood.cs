44027bf9e0ab33adc85bd22765813dba6c70e46e
SpeedAmigo <Kxipi7@gmail.com>
2025-06-11 19:37:47 +0200

    Kitchen minigame scene change

--- Assets/CounterFood.cs
+++ Assets/CounterFood.cs
@@ -1,10 +1,12 @@
 using System;
+using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class CounterFood : MinigameAbstract
 {
     public bool continousUpdate = false;
+    public bool useCuttingBoard = true;
     
     private bool isHeld = false;
     
@@ -32,8 +34,26 @@ public class CounterFood : MinigameAbstract
     
     public override void OnPointerUp(PointerEventData eventData)
     {
+        if (useCuttingBoard)
+        {
+            CheckForCuttingBoard();
+        }
+        
         isHeld = false;
         renderer.sortingOrder--;
+    }
+
+    private void CheckForCuttingBoard()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.localPosition, Vector2.zero, 0.1f);
+
+        Debug.DrawRay(transform.position, Vector2.zero, Color.red);
+        
+        if (hit.collider.TryGetComponent<ChoppingBoardScript>(out var board))
+        {
+            transform.localPosition = board.transform.position;
+            Debug.Log("Board");
+        }
     }
     
     private void LateUpdate()
